9f58e2b81b58b9f3255798ebc76342fbe5584826
chrosslaw <Chrosslaw@gmail.com>
2023-04-17 20:52:05 -0500

    styling changes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,10 +15,34 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      {/* <Header />
       <main className={styles.main}>
-        <div></div>
-      </main> */}
+        <div className={styles.description}></div>
+
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/C.svg"
+            alt="Next.js Logo"
+            width={500}
+            height={500}
+            priority
+          />
+        </div>
+
+        <div className={styles.grid}>
+          <a
+            href="https://blog.chrosstech.com"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Blog <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}></p>
+          </a>
+        </div>
+      </main>
     </>
   );
 }
